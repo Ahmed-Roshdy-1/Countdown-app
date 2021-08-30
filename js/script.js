eeaac031ceb68292f77dcd5d5e86a2b6d1d1ec48
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-08-30 17:36:50 +0200

    Populate Countdown

--- js/script.js
+++ js/script.js
@@ -2,13 +2,48 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const deleEl = document.getElementById("date-picker");
 
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
+
 let countdownTitle = "";
 let CountdownDate = "";
 let countdownValue = Date;
 
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split("T")[0];
 deleEl.setAttribute("min", today);
+
+//  Populate Countdown / Complete UI
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+  console.log(distance);
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+  console.log(days, hours, minutes, seconds);
+
+  //  Populate Countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+
+  // Hide Input
+  inputContainer.hidden = true;
+  // Show Countdown
+  countdownEl.hidden = false;
+}
 
 // Take Values from Form Input
 function updateCountdown(e) {
@@ -20,6 +55,7 @@ function updateCountdown(e) {
   // Get number version of current Date , updateDOM
   countdownValue = new Date(CountdownDate).getTime();
   console.log(countdownValue);
+  updateDOM();
 }
 
 // Event Listeners
